6d6b267bd3725ca34aa41c7000bc37beb8d671a3
Jake Luecke <jrluecke95@gmail.com>
2021-02-03 15:36:57 -0500

    finished exercises

--- exercises/level3.js
+++ exercises/level3.js
@@ -1,24 +1,32 @@
 function lvl3exercise1 () {
   // Create a "hello" and a "world", return the concatenation of the two
-
+  const hello = "hello";
+  const world = "world";
+  return hello + world
 }
 
 function lvl3exercise2 () {
   // Create a "hello" and a 12, return the concatenation of the two
-
+  const hello = "hello";
+  const num = 12;
+  return hello + num;
 }
 
 function lvl3exercise3 () {
   // Create a variable that equals 12 and convert it to a string with concatenation. Return it.
-
+  const num = 12;
+  const numToString = 12 + "";
+  return numToString;
 }
 
 function lvl3exercise4 () {
   // Create a "hello world!" string. Return the length of the string
-
+  const string = "hello world!";
+  return string.length;
 }
 
 function lvl3exercise5 () {
   // Create a "hello world!" string. Return the index of the word "world".
-
+  const string = "hello world!";
+  return string.indexOf('world');
 }
--- exercises/level7.js
+++ exercises/level7.js
@@ -3,3 +3,13 @@
 // times as the input number
 //
 // Name the function "finalFunction"
+
+function finalFunction(num) {
+    let arr = [];
+    const string = "hi";
+    let x;
+    for (x = 0; x < num; x++) {
+        arr.push(string);
+    }
+    return arr;
+}
